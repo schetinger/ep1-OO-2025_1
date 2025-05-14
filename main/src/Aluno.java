c6a7e62c3f1662fec4e2b1f3bf522aa4462aaebe
schetinger <rafaelschetinger1@gmail.com>
2025-05-14 16:15:17 -0300

    modificação da classe aluno e gravar aluno implementação de objetos

--- main/src/Aluno.java
+++ main/src/Aluno.java
@@ -3,12 +3,44 @@ import java.util.Scanner;
 
 public class Aluno {
     private String nome,curso;
-    private int matricula, disciplinas;
- //   public void addaluno(String nome, String curso, int matricula){
- //       this.nome = nome;
- //       this.curso = curso;
- //       this.matricula = matricula;
-// }
+    private int matricula;
+    private boolean especial;
+
+    public int getMatricula(){
+   
+    return matricula;
+}
+public String getNome(){
+    return nome;
+}
+public String getcurso(){
+    return curso;
+}
+public boolean getEspecial(){
+    return especial;
+}
+public void setMatricula(int matricula){
+    this.matricula=matricula;
+}
+public void setNome(String nome){
+    this.nome=nome;
+}
+public void setCurso(String curso){
+    this.curso =curso;
+}
+public void setEspecial(boolean especial){
+    this.especial=especial;
+}
+public String toString(){
+ String resumo = nome+","+matricula+","+curso;
+    return resumo;
+}
+public void fromString(String linha){
+    String[] partes= linha.split(",");
+    this.nome = partes[0];
+    this.matricula = Integer.parseInt(partes[1]);
+    this.curso = partes[2];
+}
 
     public static void aop(int opcao){
        Scanner sc = new Scanner(System.in);
@@ -16,10 +48,10 @@ public class Aluno {
         "-0- Cadastrar alunos \n"+
         "-1- Editar alunos \n"+
         "-2- Listar alunos cadastrados \n"+
-        "-3- Matricular Aluno \n"+
+        "-3- Matricular Aluno em disiplina\n"+
         "-4- Trancar disciplinas e semestre\n"+
         "-5- Voltar para o menu principal \n");
-         opcao = sc.nextInt();
+        opcao = sc.nextInt();
     switch (opcao) {
         case 0:
             GravarAluno.addaluno();
@@ -31,7 +63,9 @@ public class Aluno {
         LerAluno.listarAlunos();
             break;
         case 5:
+        sc.close();
         return;
+
         default:
         System.out.println("Opção inválida");
             break;
--- main/src/GravarAluno.java
+++ main/src/GravarAluno.java
@@ -5,57 +5,68 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class GravarAluno{
+public class GravarAluno extends Aluno{
 
         private static final String arquivo = "bancodados.csv";
     public static void addaluno(){
-        int op =0,x=1;
+        int op =0;
+        GravarAluno aluno = new GravarAluno();
     while (op ==0) {
-        x=1;
-        String[] info = dados().split(",");
+        aluno.fromString(aluno.dados());
 try (BufferedReader leitor = new BufferedReader(new FileReader(arquivo))) {
             String linha;
-                while ((linha = leitor.readLine())!= null && x==1 ) {
+                while ((linha = leitor.readLine())!= null  ) {
                     String[] partes = linha.split(",");
-
-        if(info[1].equals(partes[1])){
+        if(aluno.getMatricula()==Integer.parseInt(partes[1])){
             System.out.println("Essa matricula ja existe deseja adicionar outra matricula ou sair?\n"+
                                 "-0- Adicionar aluno novamente\n"+
                                 "-1- Sair do cadastro de alunos");
             Scanner sc = new Scanner(System.in);
             op = sc.nextInt();
-            x=0;
             if(op==1){
-                return;
-            }
-
-            }else{
-            try(BufferedWriter escrever = new BufferedWriter(new FileWriter(arquivo,true))){
-            escrever.write(info[0]+","+info[1]+","+info[2]+ "\n");
-            System.out.println("Aluno adicionado ao banco de dados com sucesso");
-        }catch (IOException e){
-            System.out.println("Algo deu errado, tente novamente");
-            }
             return;
-        }
+            }
     }
-}   catch (IOException e) {
+}
+op=1;
+}catch (IOException e) {
         e.printStackTrace();
         }
-    }        
+}
+                try(BufferedWriter escrever = new BufferedWriter(new FileWriter(arquivo,true))){
+            escrever.write(aluno.toString()+ "\n");
+            System.out.println("Aluno adicionado ao banco de dados com sucesso");
+            
+        }catch (IOException e){
+            System.out.println("Algo deu errado, tente novamente");
+            }     
 }
 
 
-public static String dados(){
-    System.out.println("Insira os dados do aluno no formato:nome,matricula,curso");
+public  String dados(){
+    
+    System.out.println("Insira o nome do aluno");
     Scanner sc = new Scanner(System.in);
-    String dados = sc.nextLine();
+    GravarAluno aluno = new GravarAluno();
+    aluno.setNome(sc.nextLine());
+    System.out.println("Insira a matrícula do aluno");
+    aluno.setMatricula(sc.nextInt());
+    System.out.println("Insira o curso do aluno");
+    sc.nextLine();
+    aluno.setCurso(sc.nextLine());
     System.out.println("Esse aluno é especial?");
-    String esp = sc.nextLine();
-    return dados;
+    
+    String a =sc.nextLine();
+    if(a.equalsIgnoreCase("sim")){
+        aluno.setEspecial(true);
+    }else{
+        aluno.setEspecial(false);
+    }
+    
+    return aluno.toString();
+}
 }
 
-}
 
     
 
